040da5ccd0d857144b0ff4e3fe7508500bfbc459
aayanraja2015 <51330235+aayanraja2015@users.noreply.github.com>
2019-06-06 11:30:08 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,1 +1,2 @@
-dart --enable-asserts --enable-vm-service:`awk -v min=10000 -v max=40000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'` src/main.dart $1
+dart --disable
+-asserts --enable-vm-service:`awk -v min=10000 -v max=40000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'` src/main.dart $1
